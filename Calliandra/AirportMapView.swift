719a0841cf0e87bc6a5c902c66e11dc333d99183
Poren Chiang <poren@poren.tw>
2025-03-21 20:49:18 +0800

    Implement selection scroll and set map style

--- Calliandra/AirportMapView.swift
+++ Calliandra/AirportMapView.swift
@@ -9,24 +9,35 @@ import CoreLocation
 struct AirportMapView: View {
     @StateObject var model = Model()
     @State private var position: MapCameraPosition = .automatic
-    @State private var selectedAirport: Airport?
+    @State private var selection: Int?
 
     var body: some View {
         Map(position: $position, interactionModes: .all) {
-            ForEach(model.airports) { airport in
+            ForEach(0..<model.airports.count) { i in
+                let airport = model.airports[i]
                 let isMajor = (model.flightsByOrigin[airport.id]?.count ?? 0 > 6)
-                let isSelected = (selectedAirport == airport)
+                let isSelected = (i == selection)
                 Annotation(airport.name, coordinate: airport.coordinate) {
                     Image(systemName: "airplane.circle.fill")
                         .foregroundStyle(.primary)
                         .imageScale((isMajor || isSelected) ? .large : .small)
                         .symbolRenderingMode(isSelected ? .multicolor : .hierarchical)
                         .onTapGesture {
-                            selectedAirport = airport
+                            selection = i
+                            position = .item(MKMapItem(placemark: MKPlacemark(coordinate: airport.coordinate)), allowsAutomaticPitch: true)
                         }
                 }
             }
         }
+        .mapControls {
+            MapZoomStepper()
+        }
+        .mapStyle(.standard(
+            elevation: .realistic,
+            emphasis: .muted,
+            pointsOfInterest: .including([.museum, .castle, .fortress, .landmark, .nationalMonument, .nationalPark, .amusementPark, .aquarium, .beach, .park, .zoo, .hiking, .publicTransport]),
+            showsTraffic: false
+        ))
     }
 }
 
